3a0810107540a04528ff7b409429184e3f117d87
Duc Nguyen <ducnafs1579@gmail.com>
2025-03-20 19:50:22 +0700

    Vong lap JS

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
-const year = parseInt(prompt("Nhập năm:"));
-const month = parseInt(prompt("Nhập tháng (1-12):"));
+// const year = parseInt(prompt("Nhập năm:"));
+// const month = parseInt(prompt("Nhập tháng (1-12):"));
+//
+//
+// if (month >= 1 && month <= 12 && year > 0) {
+//     let dateInMonth;
+//     // Check xem co phai thang 2 ko
+//     if (month === 2) {
+//         // neu la nam nhuan thi in ra 29
+//         if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+//             daysInMonth = 29;
+//         } else {
+//             daysInMonth = 28;
+//         }
+//     } else if (month === 4 || month === 6 || month === 9 || month === 11) {
+//         daysInMonth = 30;  // Tháng 4, 6, 9, 11 có 30 ngày
+//     } else {
+//         daysInMonth = 31;
+//     }
+//     alert(`Số ngày trong tháng ${month} của năm ${year} là: ${daysInMonth}`);
+// } else {
+//     alert("Dữ liệu nhập vào không hợp lệ.");
+// }
 
+const inputNumberStart = parseInt(prompt("Nhập số đầu tiên"));
+if (inputNumberStart < 2) {
+    alert("Khong phai so nguyen to")
+} else if (inputNumberStart === 2 || inputNumberStart === 3) {
+    alert("La so nguyen to")
 
-if (month >= 1 && month <= 12 && year > 0) {
-    let dateInMonth;
-    // Check xem co phai thang 2 ko
-    if (month === 2) {
-        // neu la nam nhuan thi in ra 29
-        if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
-            daysInMonth = 29;
-        } else {
-            daysInMonth = 28;
-        }
-    } else if (month === 4 || month === 6 || month === 9 || month === 11) {
-        daysInMonth = 30;  // Tháng 4, 6, 9, 11 có 30 ngày
-    } else {
-        daysInMonth = 31;
-    }
-    alert(`Số ngày trong tháng ${month} của năm ${year} là: ${daysInMonth}`);
+} else if (inputNumberStart % 2 === 0 || inputNumberStart % 3 === 0) {
+    alert("Khong phai so nguyen to")
+
 } else {
-    alert("Dữ liệu nhập vào không hợp lệ.");
+    for (let i = 5; i <= inputNumberStart; i++) {
+        if (inputNumberStart % i === 0) {
+            alert("Khong phai so nguyen to")
+            break
+        } else {
+            alert("La so nguyen to")
+            break
+        }
+    }
 }
 // var a = "hello";
 // var x = 2;
